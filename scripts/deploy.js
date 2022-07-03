792e41f8c2e15ee9432a864d4a17961014f45361
Totiruzi <totiruzi@yahoo.com>
2022-07-04 01:51:07 +0300

    Add verification function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 // imports
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 // async main
 async function main() {
@@ -10,6 +10,28 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.deployed()
   console.log(`Deployed contract to: ${simpleStorage.address}`)
+
+  // What happens when we deploy to our hardhat network
+  console.log(network.config)
+  if(network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
+}
+
+async function verify(contracAdress, args) {
+  console.log("Verifying contract .....")
+
+  try {
+    await run("verify:verify", {
+      address: contracAdress,
+      constructorArguments: args,
+    })
+  } catch (error) {
+    error.message.toLowercase().includes("already verified")
+      ? console.log("Already verified")
+      : console.log(error)
+  }
 }
 
 // main
